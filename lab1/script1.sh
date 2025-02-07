b3534b26321e1f1581b2d3fe288cbdc01703db7a
dimonlimon <dimabelunin777@niuitmo.ru>
2025-02-07 17:50:47 -0500

    12 steps are complete

--- lab1/script1.sh
+++ lab1/script1.sh
@@ -15,6 +15,24 @@ echo "dimon limon"
 # 3) --------------------------------------------------------------------
 echo "groups: $(awk -F: '{print $1}' /etc/group | tr '\n' ', ')" >> "work3.log"
 
-# 4,5,6) ------------------------------------------------------------------
+# 4,5,6,7) ------------------------------------------------------------------
 sudo useradd -m -k /home/dimonlimon/is-linux-y26/lab1/useradd_dir u1
-sudo passwd u1 | echo "123"
+echo "u1:123" | sudo chpasswd
+sudo groupadd g1
+sudo groupmod -U u1 -a g1
+
+# 8) -------------------------------------------------------------------
+user_id=$(id -u u1)
+group_info=$(id -Gn u1 | tr ' ' ', ')
+echo "u1 (id: $user_id) belongs to groups: $group_info" >> work3.log
+
+# 10) -----------------------------------------------------------------
+users_in_g1=$(getent group g1 | awk -F: '{print $4}')
+echo "Users in group g1: $users_in_g1" >> work3.log
+
+# 11) ----------------------------------------------------------------
+sudo usermod -s /usr/bin/mc u1
+
+# 12) ---------------------------------------------------------------
+sudo useradd -m u2
+echo "u2:321" | sudo chpasswd
