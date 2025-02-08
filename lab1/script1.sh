8f535c0c451eff0ecb9a94f988c8dd9ecd2d53fa
dimonlimon <dimabelunin777@niuitmo.ru>
2025-02-08 03:36:58 -0500

    all steps completed

--- lab1/script1.sh
+++ lab1/script1.sh
@@ -34,15 +34,39 @@ echo "adding new shell"
 
 # check if /usr/bin/mc already exists in a file
 echo "/usr/bin/mc" | sudo tee -a /etc/shells # add new shell to list of allowed shells
-sudo usermod -s /usr/bin/mc u1 # somehow delete entry from shells list
+# sudo usermod -s /usr/bin/mc u1 # somehow delete entry from shells list
 
 # 12) ---------------------------------------------------------------
 sudo useradd -m u2 # userdel u2; sudo rm -rf u2
 echo "u2:321" | sudo chpasswd
 
 # 13) ----------------------------------------------------------------
-sudo cp work3.log /home/work3-1.log # sudo rm -rf /home/work3-1.log
-sudo cp work3.log /home/work3-2.log # sudo rm -rf /home/work3-2.log
+sudo mkdir -p /home/test13
+sudo cp work3.log /home/test13/work3-1.log
+sudo cp work3.log /home/test13/work3-2.log
 
 # 14) -------------------------------------------------------------------
-sudo chown u1:u1 /home/test13
+sudo groupadd testgroup
+sudo usermod -aG testgroup u1
+sudo usermod -aG testgroup u2
+sudo chown u1:testgroup /home/test13
+sudo chmod u+rw,g+rx,o-rwx /home/test13
+sudo chmod u+rw,g+r,o-r /home/test13/*
+
+# 15) -----------------------------------------------------------
+sudo mkdir -p /home/test14
+sudo chown u1:u1 /home/test14
+sudo chmod u+rwx,g+rwx,o+rwx,+t /home/test14
+
+# 16) -----------------------------------------------------
+sudo cp /bin/nano /home/test14/nano
+sudo chmod u+rwx,g+rx,o+rx /home/test14/nano
+
+# 17) ------------------------------------------------------------------
+sudo mkdir -p /home/test15
+sudo touch /home/test15/secret_file
+sudo chmod u+rwx,g-rwx,o-rwx /home/test15
+sudo chmod u+rw,g-rwx,o-rwx /home/test15/secret_file
+
+# 18) -----------------------------------------------------------
+echo "u1 ALL=(ALL) NOPASSWD: /usr/bin/passwd" | sudo tee -a /etc/sudoers.d/u1-passwd
--- lab1/undo_script1.sh
+++ lab1/undo_script1.sh
@@ -3,10 +3,22 @@
 sudo userdel u1
 sudo userdel u2
 sudo groupdel g1
+sudo groupdel testgroup
 sudo rm -rf /home/u1
 sudo rm -rf /home/u2
 
 sudo sed -i '/^u1:/d' /etc/passwd
 sudo sed -i '/^u2:/d' /etc/passwd
 
+sudo chmod -R u-rwx,g-rwx,o-rwx /home/test13
+sudo rm -rf /home/test13
+
+sudo rm -f /etc/sudoers.d/u1-passwd
+sudo sed -i '/\/usr\/bin\/mc/d' /etc/shells
+sudo rm -f /etc/skel/readme.txt
+
+sudo rm -rf /home/test13
+sudo rm -rf /home/test14
+sudo rm -rf /home/test15
+sudo rm -f /home/test14/nano
 
